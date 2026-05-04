14506a3c0a1f1b9fc8ddeffcb9eec1c6612883b4
PREDATOR16neo <gpredator1@outlook.com>
2026-05-04 14:03:23 +0700

    update: terbaru

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[AuthController::class, 'login'])->name('login');
 Route::post('/action/login',[AuthController::class, 'actionLogin'])->name('action.login');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 // Routing admin
 Route::get('/admin', function () {
     return view('admin.index');
